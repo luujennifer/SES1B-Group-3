3594a69aafd150af7f3884bd81e76fc5a73789ef
kensonlang28 <62220283+kensonlang28@users.noreply.github.com>
2020-09-27 21:04:45 +1000

    Update index.php

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
 <html> 
 	<script src="https://kit.fontawesome.com/56b24aa4ed.js" crossorigin="anonymous"></script>
 	<head>
+<<<<<<< Updated upstream
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script> <!-- Imports JQuery librry -->
         <script src="Javascript%20Files/KeepFloating.js"></script> <!-- Script for keeping labels afloat -->
 
@@ -11,21 +12,36 @@
 		<link rel="stylesheet" href="CSS Files/WebsiteStyling.css"> <!-- Skeleton css file -->
 		<link rel="stylesheet" href="CSS Files/LoginStyling.css"> <!-- Login css file -->
         <link rel="stylesheet" href="CSS Files/FloatingLabels.css"> <!-- Floating labels css file -->
+=======
+		<title>Bookshelf</title> <!-- This is the title of the site that shows up in the tab feel free to change it -->
+		<link rel="stylesheet" href="CSS Files/WebsiteStyling.css"> <!-- Skeleton css file -->
+		<link rel="stylesheet" href="CSS Files/LoginStyling.css"> <!-- Login css file -->
+>>>>>>> Stashed changes
 		<link href='https://fonts.googleapis.com/css?family=Armata' rel='stylesheet'> <!-- Google font file -->
 		<link rel="icon" type="image/x-icon" href="Misc Files/logo.ico"/> <!-- icon file -->
 	</head>	
 	<body>
 		<!-- fixed top navigation bar -->
+<<<<<<< Updated upstream
 		<header style="position: fixed; z-index: 100;">
 			<div class="navigation" > 
 				<a onclick="window.location.href='index.php'"><img src="Misc Files/logo(colour).png"/><b> Bookshelf</b></a>
+=======
+		<header>
+			<div class="navigation" > 
+				<a onclick="window.location.href='index.html'"><img src="Misc Files/logo(colour).png"/><b> Bookshelf</b></a>
+>>>>>>> Stashed changes
 			</div>
 		</header>
 		
 		<!-- content body of website -->
 		<div class="body">
 			<section class="contentContainer">
+<<<<<<< Updated upstream
 				<form class="login" action="HTML Files/process.php" method="POST">
+=======
+				<form class="login" action="" method="POST"> 
+>>>>>>> Stashed changes
 					<center>
 						<label for="loginTitle" id="loginTitle"><b>Welcome Back!</b></label>
 						<p id="signinText">Sign in to continue where you left off.</p>
@@ -33,6 +49,7 @@
 					
 					<br>
 
+<<<<<<< Updated upstream
                     <div class="floating-label-wrap">
                         <input type="email" id="email" pattern="[a-zA-Z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$" required>
                         <label for="email" class="floating-label">Email</label>
@@ -41,6 +58,10 @@
                         <input type="password" id="psw" required>
                         <label for="psw" class="floating-label">Password</label>
                     </div>
+=======
+					<input type="email" placeholder="Email" name="email">
+					<input type="password" placeholder="Password" name="password">
+>>>>>>> Stashed changes
 					
 					<br>
 					
@@ -51,15 +72,24 @@
 					
 					<br>
 					
+<<<<<<< Updated upstream
 					<button id="loginBtn" type="submit"><b>Login</b></button>
 					<a id="signupBtn" href="HTML Files/bookshelf-signup.html"><b>Sign Up</b></a>
+=======
+					<button id="loginBtn" type="submit"><b>LOGIN</b></button>
+					<a id="signupBtn" href="HTML Files/bookshelf-signup.html"><b>SIGN UP</b></a>
+>>>>>>> Stashed changes
 				</form>
 			
 			</section>
 		</div>
 		
 		<!-- fixed footer bar -->
+<<<<<<< Updated upstream
 		<div class="footer" style="position: fixed; z-index: 100;">
+=======
+		<div class="footer">
+>>>>>>> Stashed changes
 			<div class="etcDetails">
 				<a href="https://github.com/luujennifer/SES1B-Group-3"><i class="fab fa-github"></i> GitHub</a>
 				<a href="https://drive.google.com/drive/u/2/folders/0ALuTuEVm4VA2Uk9PVA"><i class="fab fa-google-drive"></i> Google Drive</a>
