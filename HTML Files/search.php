3eed0391e866b3c9b0749c30d8cafba3e8cd2c0f
kensonlang28 <62220283+kensonlang28@users.noreply.github.com>
2020-09-27 21:48:03 +1000

    fixed issues with regisstration

--- HTML Files/search.php
+++ HTML Files/search.php
@@ -1,4 +1,4 @@
-<!-- BASIC WEBSITE SKELETON -->
+<!-- search form page -->
 
 <!DOCTYPE html>
 <html> 
@@ -6,9 +6,41 @@
 	<head>
 		<title>Bookshelf</title> <!-- This is the title of the site that shows up in the tab feel free to change it -->
 		<link rel="stylesheet" href="WebsiteStyling.css"> <!-- Skeleton css file -->
-		<link rel="stylesheet" href="SearchStyling.css"> <!-- Skeleton css file -->
+		<link rel="stylesheet" href="SearchStyling.css"> <!-- Search css file -->
 		<link href='https://fonts.googleapis.com/css?family=Armata' rel='stylesheet'> <!-- Google font file -->
 		<link rel="icon" type="image/x-icon" href="/logo.ico"/> <!-- icon file -->
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+		<script>
+			$(document).ready(function() {
+
+			  // Save all selects' id in an array 
+			  // to determine which select's option and value would be changed
+			  // after you select an option in another select.
+			  var selectors = ['category', 'topic']
+
+			  $('#type').on('change', function() {
+				var index = selectors.indexOf(this.id)
+				var value = this.value
+
+				// check if is the last one or not
+				if (index < selectors.length - 1) {
+				  var next = $('#' + selectors[index + 1])
+
+				  // Show all the options in next select
+				  $(next).find('option').show()
+				  if (value != "") {
+					// if this select's value is not empty
+					// hide some of the options 
+					$(next).find('option[data-value!=' + value + ']').hide()
+				  }
+				  
+				  // set next select's value to be the first option's value 
+				  // and trigger change()
+				  $(next).val($(next).find("option:first").val()).change()
+				}
+			  })
+			});
+		</script>
 	</head>	
 	<body>
 		<!-- fixed top navigation bar -->
@@ -26,10 +58,57 @@
 		<div class="body">
 			<section class="contentContainer">
 				<form class="search" action="search_results.php" method="GET">
-					<input type="text" id="search-bar" name="query" placeholder=""/>
-					<input type="submit" value="Search"/>
-					<!-- <a onclick="searchPosts(0)" href="#"><i id="search-icon" class="fas fa-search"></i></a> -->
-					<!-- <button onclick="searchPosts(0)" href="#"><i id="search-icon" class="fas fa-search"></i>Search</button> -->
+					<br>
+					<label for="searchtitle" id="searchtitle"><b>Search for Materials</b></label>
+					<p>Type to search or enter * to view all materials.</p>
+					<input type="text" id="search-bar" name="query" placeholder="Enter the material title or author to search"/>
+					<p>Or filter by category and/or topic</p>
+					<center>
+						<div>
+							<div id="type">
+								<select id="selectBox" name="category">
+									<option value="" disabled selected hidden>Choose Category</option>
+									<option value="Fiction">Fiction</option>
+									<option value="Non-Fiction">Non-Fiction</option>
+								</select>
+							</div>	
+							<div id="type">
+								<select id="selectBox" name="topic">
+									<option data-value="Non-Fiction" value="" disabled selected hidden>Choose Topic</option>
+									<option data-value="Non-Fiction" value="Architecture">Architecture</option>
+									<option data-value="Non-Fiction" value="Built Environment">Built Environment</option>
+									<option data-value="Non-Fiction" value="Business">Business</option>
+									<option data-value="Non-Fiction" value="Communication">Communication</option>
+									<option data-value="Non-Fiction" value="Design">Design</option>
+									<option data-value="Non-Fiction" value="Education">Education</option>
+									<option data-value="Non-Fiction" value="Engineering">Engineering</option>
+									<option data-value="Non-Fiction" value="Health">Health</option>
+									<option data-value="Non-Fiction" value="Indigenous Studies">Indigenous Studies</option>
+									<option data-value="Non-Fiction" value="Information Technology">Information Technology</option>
+									<option data-value="Non-Fiction" value="International Studies">International Studies</option>
+									<option data-value="Non-Fiction" value="Law">Law</option>
+									<option data-value="Non-Fiction" value="News">News</option>
+									<option data-value="Non-Fiction" value="Science">Science</option>
+									<option data-value="Fiction" value="Modern & Contemporary Fiction">Modern &amp; Contemporary Fiction</option>
+									<option data-value="Fiction" value="Poetry & Drama">Poetry &amp; Drama</option>
+									<option data-value="Fiction" value="Romance">Romance</option>
+									<option data-value="Fiction" value="Crime">Crime</option>
+									<option data-value="Fiction" value="Classic Fiction">Classic Fiction</option>
+									<option data-value="Fiction" value="Fantasy">Fantasy</option>
+									<option data-value="Fiction" value="Adventure Fiction">Adventure Fiction</option>
+									<option data-value="Fiction" value="Science Fiction">Science Fiction</option>
+									<option data-value="Fiction" value="Historical Fiction">Historical Fiction</option>
+									<option data-value="Fiction" value="Horror & Paranormal Fiction">Horror &amp; Paranormal Fiction</option>
+									<option data-value="Fiction" value="Graphic Novels">Graphic Novels</option>
+									<option data-value="Fiction" value="Young Adult">Young Adult</option>
+									<option data-value="Fiction" value="Manga">Manga</option>
+									<option data-value="Fiction" value="Dystopian">Dystopian</option>
+								</select>
+							</div>
+						</div>
+					</center>
+					<center><button id="searchbtn" type="submit"><b>SEARCH</b></button></center>
+
 				</form>
 			
 			</section>
--- HTML Files/search_results.php
+++ HTML Files/search_results.php
@@ -1,3 +1,5 @@
+<!-- search results page -->
+
 <?php
 	// mysql_connect("localhost", "root", "689iABj") or die("Error connecting to database: ".mysql_error());
 	/*
@@ -23,7 +25,7 @@
 	<head>
 		<title>Bookshelf</title> <!-- This is the title of the site that shows up in the tab feel free to change it -->
 		<link rel="stylesheet" href="WebsiteStyling.css"> <!-- Skeleton css file -->
-		<link rel="stylesheet" href="SearchStyling.css"> <!-- Skeleton css file -->
+		<link rel="stylesheet" href="SearchStyling.css"> <!-- Search css file -->
 		<link href='https://fonts.googleapis.com/css?family=Armata' rel='stylesheet'> <!-- Google font file -->
 		<link rel="icon" type="image/x-icon" href="/logo.ico"/> <!-- icon file -->
 	</head>	
@@ -43,6 +45,7 @@
 		<div class="body">
 			<section class="contentContainer">
 			<!-- return to search -->
+			<a id="returntosearch" href="../HTML Files/search.php"><i class="fas fa-caret-left"></i><!-- this is a non breaking space --> &nbsp; &nbsp; Return to Search</a>
 <?php
 	$query = $_GET['query']; 
 	// gets value sent over search form
@@ -58,9 +61,13 @@
 		// $query = mysqli_real_escape_string($conn, $query);
 		// makes sure nobody uses SQL injection
 		
-		$raw_results = mysqli_query($conn, "SELECT * FROM books
-			WHERE (`title` LIKE '%".$query."%')") or die($conn -> error);
-			
+		if($query == '*'){
+			$raw_results = mysqli_query($conn, "SELECT * FROM books") or die($conn -> error);
+		} 
+		else {
+			$raw_results = mysqli_query($conn, "SELECT * FROM books
+			WHERE (`title` LIKE '%".$query."%') OR (`author` LIKE '%".$query."%')") or die($conn -> error);
+		}
 		// * means that it selects all fields, you can also write: `id`, `title`, `text`
 		// articles is the name of our table
 		
@@ -70,23 +77,26 @@
 		
 		//$num = mysqli_num_rows($raw_results)
 		if(mysqli_num_rows($raw_results) > 0){ // if one or more rows are returned do following
-		//$num = mysqli_num_rows($raw_results)
-			//echo "<br><br><br>There are ".$num."results.";
+		$num = mysqli_num_rows($raw_results);
+		echo "<br><br>There are ".$num."&nbspmaterials that match the search criteria.<br>";
 			while($results = $raw_results->fetch_assoc()){
 			// $results = mysql_fetch_array($raw_results) puts data from database into array, while it's valid it does the loop
-			
-				echo "<br><br><br><p><h3 id='booktitle'>".$results['title']."</h3><b>By: </b>".$results['author']."&nbsp&nbsp&nbsp&nbsp<b>Topic: </b>".$results['topic']."&nbsp&nbsp&nbsp&nbsp<b>Type: </b>".$results['type']."</p>";
-				// posts results gotten from database(title and text) you can also show id ($results['id'])
+				if($results['type'] == 'Fiction'){
+					echo "<p><h3 id='booktitle'>".$results['title']."</h3><b>By: </b>".$results['author']."&nbsp&nbsp&nbsp&nbsp<b>Type: </b>".$results['type']."&nbsp&nbsp&nbsp&nbsp<b>Topic: </b>".$results['fiction_topic']."</p><br>";
+				}
+				else {
+					echo "<p><h3 id='booktitle'>".$results['title']."</h3><b>By: </b>".$results['author']."&nbsp&nbsp&nbsp&nbsp<b>Type: </b>".$results['type']."&nbsp&nbsp&nbsp&nbsp<b>Topic: </b>".$results['non_fiction_topic']."</p><br>";
+				}
 			}
 			
 		}
 		else{ // if there is no matching rows do following
-			echo "<br><br><br>No results";
+			echo "<br><br>No results";
 		}
 		
 	}
 	else{ // if query length is less than minimum
-		echo "<br><br><br>Minimum length is ".$min_length;
+		echo "<br><br>Please enter a valid search parameter.";
 	}
 ?>
 </section>
