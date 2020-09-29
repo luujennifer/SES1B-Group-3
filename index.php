157c5fc2fd2d13d1f9b464d8e4ba3a50afab353b
kensonlang28 <62220283+kensonlang28@users.noreply.github.com>
2020-09-29 18:33:59 +1000

    Changing the name fields to match the php file

--- index.php
+++ index.php
@@ -34,11 +34,11 @@
 					<br>
 
                     <div class="floating-label-wrap">
-                        <input type="email" id="email" pattern="[a-zA-Z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$" required>
+                        <input type="email" id="email" name="email" pattern="[a-zA-Z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$" required>
                         <label for="email" class="floating-label">Email</label>
                     </div>
                     <div class="floating-label-wrap">
-                        <input type="password" id="psw" required>
+                        <input type="password" id="psw" name="psw" required>
                         <label for="psw" class="floating-label">Password</label>
                     </div>
 					
