565c56770bd8f8ba902285f37b7e2df0f6db48b9
Jennifer Luu <58239721+luujennifer@users.noreply.github.com>
2020-10-04 17:57:10 +1100

    Updating associate file links for staff
    Signed-off-by: Jennifer Luu <58239721+luujennifer@users.noreply.github.com>

--- HTML Files/staff-account-settings.php
+++ HTML Files/staff-account-settings.php
@@ -69,10 +69,10 @@
 		<!-- fixed top navigation bar -->
 		<header>
 			<div class="navigation" > 
-				<a onclick="window.location.href='admin-account.html'"><img src="../Misc Files/logo(colour).png"/><b> Bookshelf</b></a>
+				<a onclick="window.location.href='../HTML Files/staff-account.html'"><img src="../Misc Files/logo(colour).png"/><b> Bookshelf</b></a>
 				<div id="name">
-					<a id="settings" onclick="window.location.href='staff-account-settings.php'">My Account</a> <!-- linked to settings -->
-					<a id="logout" onclick="window.location.href=''"><i id="logout" class="fas fa-sign-out-alt"></i></a>
+					<a id="settings" onclick="window.location.href='../HTML Files/staff-account-settings.php'">My Account</a> <!-- linked to settings -->
+					<a id="logout" onclick="window.location.href='../index.php'"><i id="logout" class="fas fa-sign-out-alt"></i></a>
 				</div>
 			</div>
 		</header>
