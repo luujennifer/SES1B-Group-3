107499a91a62c8a9e6ba317645c7643118b06205
Jennifer Luu <58239721+luujennifer@users.noreply.github.com>
2020-10-08 00:56:49 +1100

    Fixing registration script to take page redirect and account type
    Signed-off-by: Jennifer Luu <58239721+luujennifer@users.noreply.github.com>

--- PHP Files/registrationdb.php
+++ PHP Files/registrationdb.php
@@ -13,18 +13,19 @@
 	$lastname = $_POST['LastName'];
 	$email = $_POST['Email'];
 	$password = $_POST['password'];
+	$type = $_POST['acc_type']
 
-	$sql = "INSERT INTO users (firstname, lastname, email, password) 
-			VALUES ('$firstname', '$lastname', '$email','$password')";
+	$sql = "INSERT INTO users (firstname, lastname, email, password, account_type) 
+			VALUES ('$firstname', '$lastname', '$email','$password', '$type')";
 			
 	if (!mysqli_query($conn, $sql)) {
 		echo "<script>alert('Registration failed, please try again.')</script>";
-		include("../HTML Files/bookshelf-signup.html");
+		echo "<script>location.replace('../HTML Files/bookshelf-signup.html')</script>";
 	}
 	
 	else {
 		echo "<script>alert('Registration successful, you may login.')</script>";
-		include("../index.php")
+		echo "<script>location.replace('../index.php')</script>";
 	}
 
 ?>
