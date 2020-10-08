a9ba32519580b4fdc5e5ccde60b93d133f5f0730
Jennifer Luu <58239721+luujennifer@users.noreply.github.com>
2020-10-09 00:32:29 +1100

    Fixing borrow materials script to work for student and staff
    Signed-off-by: Jennifer Luu <58239721+luujennifer@users.noreply.github.com>

--- PHP Files/borrowMaterials.php
+++ PHP Files/borrowMaterials.php
@@ -17,15 +17,29 @@
 
 	$sql = "INSERT INTO loans (email, book_id, book_title, book_author, fee) 
 			VALUES ('$email', '$book_id', '$book_title','$book_author', '$fee')";
+	
+	$search = "SELECT * FROM users WHERE email = '$email'";
+	$result = mysqli_query($conn,$sql);
+	$row = mysqli_fetch_array($result);
+	$account_type = $row['account_type'];
 			
 	if (!mysqli_query($conn, $sql)) {
 		echo "<script>alert('Book loan failed, please try again.')</script>";
-		echo "<script>location.replace('../HTML Files/user-borrow-materials.html')</script>";
+		if($account_type == 'Student'){
+			echo "<script>location.replace('../HTML Files/user-borrow-materials.html')</script>";
+		} else if($account_type == 'Staff'){
+			echo "<script>location.replace('../HTML Files/staff-borrow-materials.html')</script>";
+		}
 	}
 	
 	else {
 		echo "<script>alert('Book loan sucessfully processed.')</script>";
-		echo "<script>location.replace('../HTML Files/User Account.html')</script>";
+		if($account_type == 'Student'){
+			echo "<script>location.replace('../HTML Files/User Account.html')</script>";
+		} else if($account_type == 'Staff'){
+			echo "<script>location.replace('../HTML Files/staff-account.html')</script>";
+		}
+		
 	}
 
 ?>
