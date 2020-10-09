ca6968f6475db25dbe67bc37ed44a1722bd59cce
kensonlang28 <62220283+kensonlang28@users.noreply.github.com>
2020-10-09 17:19:38 +1100

    Update index.php

--- index.php
+++ index.php
@@ -37,13 +37,19 @@
 					<br>
 
                     <div class="floating-label-wrap">
+<<<<<<< Updated upstream
 
+=======
+>>>>>>> Stashed changes
                         <input type="email" id="email" name="email" pattern="[a-zA-Z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$" required>
                         <label for="email" class="floating-label">Email</label>
                     </div>
                     <div class="floating-label-wrap">
                         <input type="password" id="psw" name="psw" required>
+<<<<<<< Updated upstream
 
+=======
+>>>>>>> Stashed changes
                         <label for="psw" class="floating-label">Password</label>
                     </div>
 					
