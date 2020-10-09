0e857f5fcd6c93504ed2340737d98f7b62617434
Jennifer Luu <58239721+luujennifer@users.noreply.github.com>
2020-10-09 22:20:51 +1100

    Splitting borrow script into separate doc for staff and student
    Signed-off-by: Jennifer Luu <58239721+luujennifer@users.noreply.github.com>

---
+++ PHP Files/staffBorrowMaterials.php
@@ -0,0 +1,35 @@
+<?php
+	// STAFF BORROW MATERIALS SCRIPT 
+	
+	session_start();
+
+	$dbhost = "localhost";
+	$dbuser = "root";
+	$dbpass = "";
+	$db = "bookshelf";
+	
+	$conn = new mysqli($dbhost, $dbuser, $dbpass, $db);
+
+	$email = $_POST['email'];
+	$book_id = $_POST['book_id'];
+	$book_title = $_POST['book_title'];
+	$book_author = $_POST['book_author'];
+	$fee = '12';
+
+	$sql = "INSERT INTO loans (email, book_id, book_title, book_author, fee) 
+			VALUES ('$email', '$book_id', '$book_title','$book_author', '$fee')";
+			
+	if (!mysqli_query($conn, $sql)) {
+		echo "<script>alert('Book loan failed, please try again.')</script>";
+		echo "<script>location.replace('../HTML Files/staff-borrow-materials.html')</script>";
+		
+	}
+	
+	else {
+		echo "<script>alert('Book loan sucessfully processed.')</script>";
+		echo "<script>location.replace('../HTML Files/staff-account.html')</script>";
+		
+		
+	}
+
+?>
