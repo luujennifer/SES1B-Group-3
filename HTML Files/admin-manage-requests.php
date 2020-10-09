d449a4deaabb35faed222646af64b52be7ac3a05
Jennifer Luu <58239721+luujennifer@users.noreply.github.com>
2020-10-10 00:24:36 +1100

    editing admin manage requests page
    Signed-off-by: Jennifer Luu <58239721+luujennifer@users.noreply.github.com>

--- HTML Files/admin-manage-requests.php
+++ HTML Files/admin-manage-requests.php
@@ -24,6 +24,8 @@
 		<link rel="stylesheet" href="../CSS Files/WebsiteStyling.css"> <!-- Skeleton css file -->
 		<link rel="stylesheet" type="text/css" href="../CSS Files/AdminStyling.css"> <!--Styling for staff account-->
 		<link rel="stylesheet" href="../CSS Files/SearchStyling.css"> <!-- Search css file -->
+		<link rel="stylesheet" href="../CSS Files/SearchResultsStyling.css"> <!-- Search css file -->
+		<link rel="stylesheet" href="../CSS Files/ManageRequestsStyling.css"> <!-- Search css file -->
 		<link href='https://fonts.googleapis.com/css?family=Armata' rel='stylesheet'> <!-- Google font file -->
 		<link rel="icon" type="image/x-icon" href="../Misc Files/logo.ico"/> <!-- icon file -->
 	</head>	
@@ -43,49 +45,33 @@
 		<div class="body">
 		<br>
 		<br>
-		<a id="returnhome" href="../HTML Files/staff-account.html"><i class="fas fa-caret-left"></i>&nbsp; &nbsp; Return to Dashboard</a>
+		<a id="returnhome" href="../HTML Files/admin-account.html"><i class="fas fa-caret-left"></i>&nbsp; &nbsp; Return to Dashboard</a>
 			<section class="contentContainer">
-				<h1>Manage my Loans</h1>
-				<form action="" method=""> <!-- need to write script to search the table below -->
-					<div id="search-container">
-						<input type="text" id="search-bar" name="query" placeholder="Enter * to view all or search via material title or author"/>
-						<button type="submit"><i class="fas fa-search"></i></button>
-					</div>
-				</form>
-			
-				<table>
-					<tr>
-						<th>Request ID</th>
-						<th>Author</th>
-						<th>Title</th>
-						<th>ISBN</th>
-						<th>Category</th>
-						<th>Format</th>
-					</tr>
+				<h1 id="manageRequestsTitle">Manage Material Requests</h1>
+				<?php
+					$location = '"index.html"'; // NEED TO CHANGE TO BE edit MATERIALS FORM 
 					
-					<!-- print all database data into a table -->
-					<?php
-						if(mysqli_num_rows($results)) //result greater than 0
-						{
-							while($results->fetch_assoc()) //fetch associate
-							{
-								echo'<tr>
-										<td>'.$row['request_id'].'</td>
-										<td>'.$row['author'].'</td>
-										<td>'.$row['title'].'</td>
-										<td>'.$row['ISBN'].'</td>
-										<td>'.$row['type'].'</td>
-										<td>'.$row['format'].'</td>
-									</tr>';
+					$raw_results = mysqli_query($conn, "SELECT * FROM book_request ORDER BY request_id") or die($conn -> error);
+						
+
+						if(mysqli_num_rows($raw_results) > 0){ // if one or more rows are returned do following
+						$num = mysqli_num_rows($raw_results);
+						echo "<p id='message'>There are <b>".$num."</b>&nbspmaterial requests.</p><br>";
+							while($results = $raw_results->fetch_assoc()){
+							// $results = mysql_fetch_array($raw_results) puts data from database into array, while it's valid it does the loop
+								
+								echo "<a id='booktitle' style='cursor:pointer;' onclick='window.location.href=".$location."'><h3>".$results['title']."</h3></a><p id='details'><b>By: </b>".$results['author']."&nbsp&nbsp&nbsp&nbsp<b>Type: </b>".$results['type']."&nbsp&nbsp&nbsp&nbsp<b>ISBN: </b>".$results['ISBN']."</p><br>";
+								
+								
 							}
+							echo "<p id='message'>End of material requests.</p><br><br>";
 							
 						}
-						else 
-						{
-							echo "<br><br>No current loans.<br><br>";
+						else{ // if there is no matching rows do following
+							echo "<br><p id='message'>No material requests.</p>";
 						}
-					?>
-				</table>
+						
+				?>
 			
 			</section>
 		</div>
