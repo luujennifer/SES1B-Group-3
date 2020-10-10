f7fb2449bde358dcb164554dbd149c2035ddbf46
Jennifer Luu <58239721+luujennifer@users.noreply.github.com>
2020-10-10 19:45:05 +1100

    adding manage materials script
    Signed-off-by: Jennifer Luu <58239721+luujennifer@users.noreply.github.com>

---
+++ PHP Files/manageRequest.php
@@ -0,0 +1,39 @@
+<?php
+	// SIGN UP SCRIPT
+	
+
+
+	$dbhost = "localhost";
+	$dbuser = "root";
+	$dbpass = "";
+	$db = "bookshelf";
+	
+	$conn = new mysqli($dbhost, $dbuser, $dbpass, $db);
+
+	$title = $_POST['title'];
+	$author = $_POST['author'];
+	$ISBN = $_POST['ISBN'];
+	$type = $_POST['category'];
+    $format = $_POST['format'];
+    $copies = '1';
+    
+    if(isset($_POST['submit'])){
+        $sql = "INSERT INTO books (author, title, ISBN, type, format, copies) 
+			VALUES ('$author', '$title', '$ISBN','$type', '$format', '$copies')";
+    } else if(isset($_POST['delete'])) { /* THIS DOESNT WORK */
+        $sql = "DELETE FROM book_request WHERE 'ISBN'=$ISBN";
+    }
+
+	
+			
+	if (!mysqli_query($conn, $sql)) {
+		echo "<script>alert('Material request was not updated, please try again.')</script>";
+		echo "<script>location.replace('../HTML Files/admin-manage-request.html')</script>";
+	}
+	
+	else {
+		echo "<script>alert('Material request updated sucessfully.')</script>";
+		echo "<script>location.replace('../HTML Files/admin-view-requests.php')</script>";
+	}
+
+?>
