4d6e27d3c7d02b23f28269abbd0a23b301302511
willhtang <will.h.tang@gmail.com>
2020-10-11 16:53:29 +1100

    Delete query working

--- PHP Files/delete-item.php
+++ PHP Files/delete-item.php
@@ -6,10 +6,15 @@ $db ="bookshelf";
 $conn = new mysqli($dbhost, $dbuser, $dbpass, $db) or die ("Error connecting to database: ". $conn -> error);
 
 $ids = $_POST['ids'];
-foreach (explode(',', $ids) as $id){
+foreach(explode(',', $ids) as $id){
+    $id = preg_replace('/[^A-Za-z0-9\-]/', '', $id);;
     $sql = "DELETE FROM books WHERE book_id = '$id'";
-    $result = mysqli_query($conn, $sql) or die($conn -> error);
-    echo $result;
+    if(mysqli_query($conn, $sql)){
+        echo "Material successfully deleted";
+    }
+    else{
+        echo ($conn -> error);
+    }
 }
 
 
